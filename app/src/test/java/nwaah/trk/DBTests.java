46df4571812e599a7561761a0e8450778618e86c
Nwaah <dkrol1@poczta.onet.pl>
2017-02-21 23:53:16 +0100

    tests init

---
+++ app/src/test/java/nwaah/trk/DBTests.java
@@ -0,0 +1,20 @@
+package nwaah.trk;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class DBTests {
+    @Test
+    public void nothing()
+    {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void fail()
+    {
+        int a =1;
+        Assert.assertTrue(a!=1);
+    }
+}
